53a8ec2da8517eb210561000c4fd09db415f21a8
Piyush Raghav <156657568+Piyushraghav01@users.noreply.github.com>
2024-12-21 19:52:09 +0530

    Add files via upload

---
+++ Arrays2d.java
@@ -0,0 +1,21 @@
+class Arrays2d{
+    public static void main(String[] args) {
+        // int arr[] = new int[5];
+        // System.out.println(arr);
+        // System.out.println(arr[0]);
+
+        //2-d Array
+        int A[][] = new int[3][4];
+        System.out.println(A);  
+        System.out.println(A[0]);
+
+        // int[][] B = new int[6][4];
+        // int []C[]  = new int[6][4];
+        // int[] D,E,F[];
+        // E = new int[5];
+        // F = new int[4][5];
+
+
+
+    }
+}
